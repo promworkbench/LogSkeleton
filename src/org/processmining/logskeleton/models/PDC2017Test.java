1debbae4dbf101528f3fba4916e41db7796a3d4e
hmwverbeek <h.m.w.verbeek@tue.nl>
2017-05-01 12:01:54 +0000

    Improved test report. Now shows the accepted and rejected traces.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/LogSkeleton/Trunk@35209 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/logskeleton/models/PDC2017Test.java
+++ src/org/processmining/logskeleton/models/PDC2017Test.java
@@ -1,9 +1,13 @@
 package org.processmining.logskeleton.models;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.deckfour.xes.extension.std.XConceptExtension;
 import org.deckfour.xes.model.XLog;
+import org.deckfour.xes.model.XTrace;
 import org.processmining.framework.util.HTMLToString;
 
 public class PDC2017Test implements HTMLToString {
@@ -13,7 +17,7 @@ public class PDC2017Test implements HTMLToString {
 	private List<XLog> mayLogs;
 	private List<XLog> juneLogs;
 	private List<LogSkeleton> models;
-	
+
 	public PDC2017Test() {
 		numbers = new ArrayList<Integer>();
 		aprilLogs = new ArrayList<XLog>();
@@ -21,25 +25,52 @@ public class PDC2017Test implements HTMLToString {
 		juneLogs = new ArrayList<XLog>();
 		models = new ArrayList<LogSkeleton>();
 	}
-	
-	public void add(int i, XLog aprilLog, XLog mayLog, XLog juneLog,LogSkeleton model) {
+
+	public void add(int i, XLog aprilLog, XLog mayLog, XLog juneLog, LogSkeleton model) {
 		numbers.add(i);
 		aprilLogs.add(aprilLog);
 		mayLogs.add(mayLog);
 		juneLogs.add(juneLog);
 		models.add(model);
 	}
-	
+
 	public String toHTMLString(boolean includeHTMLTags) {
 		StringBuffer buf = new StringBuffer();
 		if (includeHTMLTags) {
 			buf.append("<html>");
 		}
-		buf.append("<table><tr><th>Number</th><th>April</th><th>May</th><th>June</th></tr>");
-		for (int i = 0; i < numbers.size(); i++) {
-			buf.append("<tr><td>" + numbers.get(i) + "</td><td>" + aprilLogs.get(i).size() + "</td><td>" + mayLogs.get(i).size() + "</td><td>" + juneLogs.get(i).size() + "</td></tr>");
+		List<XLog> logs;
+		for (int l = 0 ; l < 3; l++) {
+			if (l == 0) {
+				buf.append("<h1>April</h1>");
+				logs = aprilLogs;
+			} else if (l == 1) {
+				buf.append("<h1>May</h1>");
+				logs = mayLogs;
+			} else {
+				buf.append("<h1>June</h1>");
+				logs = juneLogs;
+			}
+			buf.append("<table><tr><th>Log/Trace</th>");
+			for (int n = 1; n < 21; n++) {
+				buf.append("<th>" + n + "</th>");
+			}
+			buf.append("<th>#Yes</th>");
+			buf.append("</tr>");
+			for (int i = 0; i < numbers.size(); i++) {
+				Set<String> acceptedTraces = new HashSet<String>();
+				for (XTrace trace : logs.get(i)) {
+					acceptedTraces.add(XConceptExtension.instance().extractName(trace));
+				}
+				buf.append("<tr><td>" + numbers.get(i) + "</td>");
+				for (int n = 1; n < 21; n++) {
+					buf.append("<td>" + (acceptedTraces.contains("" + n) ? "<b>Yes</b>" : "<i>No</i>") + "</td>");
+				}
+				buf.append("<td>" + logs.get(i).size() + "</td>");
+				buf.append("</tr>");
+			}
+			buf.append("</table>");
 		}
-		buf.append("</table>");
 		if (includeHTMLTags) {
 			buf.append("</html>");
 		}
