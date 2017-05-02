c0a26d861894b0e0cfeff82fa3ca17cfcb33662e
hmwverbeek <h.m.w.verbeek@tue.nl>
2017-05-02 14:04:43 +0000

    Fixed typos
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/LogSkeleton/Trunk@35227 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/logskeleton/plugins/LogSkeletonCheckerPlugin.java
+++ src/org/processmining/logskeleton/plugins/LogSkeletonCheckerPlugin.java
@@ -8,7 +8,7 @@ import org.processmining.framework.plugin.annotations.PluginVariant;
 import org.processmining.logskeleton.algorithms.LogSkeletonCheckerAlgorithm;
 import org.processmining.logskeleton.models.LogSkeleton;
 
-@Plugin(name = "Filter Event Log on Log Skeleton", parameterLabels = { "Log Skeleton", "Event log"}, returnLabels = { "Filtered Event Log" }, returnTypes = { XLog.class }, userAccessible = true, help = "Filter Event Log on Lok Skeleton")
+@Plugin(name = "Filter Event Log on Log Skeleton", parameterLabels = { "Log Skeleton", "Event log"}, returnLabels = { "Filtered Event Log" }, returnTypes = { XLog.class }, userAccessible = true, help = "Filter Event Log on Log Skeleton")
 public class LogSkeletonCheckerPlugin extends LogSkeletonCheckerAlgorithm {
 
 	@UITopiaVariant(affiliation = UITopiaVariant.EHV, author = "H.M.W. Verbeek", email = "h.m.w.verbeek@tue.nl")
