e07c938b556ff07e39cd8f38efa5385191e3d449
hmwverbeek <h.m.w.verbeek@tue.nl>
2019-03-01 08:42:28 +0000

    Added plug-in to partition log on activity sets. Every partitioned log typically has a nice log skeleton.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/LogSkeleton/Trunk@41477 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/logskeleton/algorithms/LogPartitionerAlgorithm.java
@@ -0,0 +1,99 @@
+package org.processmining.logskeleton.algorithms;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.deckfour.xes.extension.std.XConceptExtension;
+import org.deckfour.xes.factory.XFactoryRegistry;
+import org.deckfour.xes.model.XAttributeList;
+import org.deckfour.xes.model.XAttributeMap;
+import org.deckfour.xes.model.XEvent;
+import org.deckfour.xes.model.XLog;
+import org.deckfour.xes.model.XTrace;
+import org.processmining.log.models.EventLogArray;
+import org.processmining.log.models.impl.EventLogArrayFactory;
+
+public class LogPartitionerAlgorithm {
+
+	private void partition(EventLogArray logs, Set<String> positiveFilters, Set<String> negativeFilters) {
+		XLog log = logs.getLog(logs.getSize() - 1);
+		XLog filteredLog = XFactoryRegistry.instance().currentDefault()
+				.createLog((XAttributeMap) log.getAttributes().clone());
+		XLog discardedLog = XFactoryRegistry.instance().currentDefault().createLog(log.getAttributes());
+		for (XTrace trace : log) {
+			boolean ok = true;
+			Set<String> toMatch = new HashSet<String>(positiveFilters);
+			for (XEvent event : trace) {
+				String activity = XConceptExtension.instance().extractName(event);
+				if (negativeFilters.contains(activity)) {
+					ok = false;
+					;
+				}
+				toMatch.remove(activity);
+			}
+			if (ok && toMatch.isEmpty()) {
+				filteredLog.add(trace);
+			} else {
+				discardedLog.add(trace);
+			}
+		}
+		logs.removeLog(logs.getSize() - 1);
+
+		logs.addLog(filteredLog);
+		logs.addLog(discardedLog);
+	}
+
+	public EventLogArray apply(XLog log) {
+		String name = XConceptExtension.instance().extractName(log);
+		Set<String> activities = new HashSet<String>();
+		for (XTrace trace : log) {
+			for (XEvent event : trace) {
+				activities.add(XConceptExtension.instance().extractName(event));
+			}
+		}
+		EventLogArray logs = EventLogArrayFactory.createEventLogArray();
+		logs.addLog(log);
+		Set<String> positiveFilters = new HashSet<String>();
+		while (!log.isEmpty()) {
+			Map<Set<String>, Double> scores = new HashMap<Set<String>, Double>();
+			double maxScore = 0;
+			Set<String> negativeFilters = new HashSet<String>();
+			for (XTrace trace : log) {
+				Set<String> score = new HashSet<String>();
+				for (XEvent event : trace) {
+					score.add((XConceptExtension.instance().extractName(event)));
+				}
+				if (!scores.containsKey(score)) {
+					scores.put(score, 1.0 / (trace.size() + 1));
+				} else {
+					scores.put(score, scores.get(score) + (1.0 / (trace.size() + 1)));
+				}
+				if (scores.get(score) > maxScore) {
+					maxScore = scores.get(score);
+				}
+			}
+			Set<String> bestScore = null;
+			XAttributeList list = XFactoryRegistry.instance().currentDefault().createAttributeList("activities", null);
+			for (Set<String> score : scores.keySet()) {
+				if (bestScore == null && scores.get(score) == maxScore) {
+					bestScore = score;
+					negativeFilters.addAll(activities);
+					negativeFilters.removeAll(score);
+					for (String s : score) {
+						list.addToCollection(XFactoryRegistry.instance().currentDefault()
+								.createAttributeLiteral(XConceptExtension.KEY_NAME, s, XConceptExtension.instance()));
+					}
+				}
+			}
+			partition(logs, positiveFilters, negativeFilters);
+			log = logs.getLog(logs.getSize() - 2);
+			XConceptExtension.instance().assignName(log, name + " @" + (logs.getSize() - 1) + " |" + log.size() + "| " + bestScore);
+			log.getAttributes().put("activities", list);
+			log = logs.getLog(logs.getSize() - 1);
+		}
+		return logs;
+	}
+
+}
