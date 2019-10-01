089977cba5ae713bad7b70288f2999a726f50f25
hmwverbeek <h.m.w.verbeek@tue.nl>
2019-10-01 09:03:15 +0000

    Updated URL.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/LogSkeleton/Trunk@42564 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/logskeleton/plugins/BrowserPlugin.java
+++ src/org/processmining/logskeleton/plugins/BrowserPlugin.java
@@ -19,7 +19,7 @@ import org.processmining.logskeleton.models.LogSkeleton;
 		returnTypes = {	JComponent.class }, //
 		userAccessible = true, //
 		icon = "prom_duck.png", //
-		url = "https://www.win.tue.nl/~hverbeek/blog/2019/09/26/log-skeleton-browser-4/", //
+		url = "https://www.win.tue.nl/~hverbeek/blog/2019/10/01/log-skeleton-browser-5/", //
 		help = "Log Skeleton Browser" //
 ) //
 @Visualizer //
